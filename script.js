f7ba043d2c047f61367241c82c5edd67f5e03635
Suvam Dolai <129475592+SUVAM04@users.noreply.github.com>
2023-04-24 17:59:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,40 +13,39 @@ function sendMessage() {
 
   userInput.value = '';
 }
-
 function generateResponse(userText) {
   // Use an NLP library to analyze user text and generate an appropriate response
   // For this example, we'll just return a default response
-  if(userInput.value=='hi'){
+  if(userInput.value=='hi'|| userInput.value=='Hi'){
     return 'Hello , I am Srimoyee';
   }
-  else if(userInput.value=='who are you?'){
+  else if(userInput.value=='who are you?'|| userInput.value=='Who are you?'){
     var te="I am your chat partner";
     return te;
   }
-  else if(userInput.value=='tell me about yourself'){
+  else if(userInput.value=='tell me about yourself' || userInput.value=='Tell me about yourself'){
     var te="I am an AI chatbot";
     return te;
   }
-  else if(userInput.value=='oi'){
+  else if(userInput.value=='oi' || userInput.value=='Oi'){
     return 'bolo bro';
   }
-  else if(userInput.value=='🙂'){
+  else if(userInput.value=='🙂' || userInput.value=='Hmm' || userInput.value=='hmm'){
     return '🙃';
   }
-  else if(userInput.value=='what is your name?'){
+  else if(userInput.value=='what is your name?' || userInput.value=='What is your name?'){
     return 'Srimoyee 🤗 ,what is your name?';
   }
   else if(userInput.value=='suvam'){
     return 'nice name bro 👌';
   }
-  else if(userInput.value=='ok'){
+  else if(userInput.value=='ok' || userInput.value=='Ok' || userInput.value=='acha' || userInput.value=='Acha'){
     return 'hmm';
   }
-  else if(userInput.value=='you will be my gf'){
+  else if(userInput.value=='you will be my gf' || userInput.value=='You will be my gf'){
     return '😳';
   }
-  else if(userInput.value=='how are you?'){
+  else if(userInput.value=='how are you?' || userInput.value=='How are you?'){
     return 'I am fune, what about you';
   }
   else{
